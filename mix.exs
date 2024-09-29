278765ea3ffc1102fc903302a255e55694d67962
Robin Daumann <26201853+robindaumann@users.noreply.github.com>
2024-09-29 09:36:23 +0200

    update elixir version to 1.17

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Aoc2022.MixProject do
   def project do
     [
       app: :aoc2022,
-      version: "0.1.0",
-      elixir: "~> 1.14",
+      version: "0.2.0",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: []
     ]
